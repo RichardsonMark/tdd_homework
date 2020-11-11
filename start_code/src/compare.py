d44f77bb5eb6d63492336e7d79c66c63d0b77fb6
Mark Richardson <mark_richardson@sky.com>
2020-11-11 18:21:35 +0000

    Refactored code to tidy - removed abbreviation and grouped related conditions with if, elif, else

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -3,10 +3,10 @@ def compare():
 
 
 # compare.py
-def compare(num_1, num_2):
-    if num_1 == num_2:
+def compare(number_1, number_2):
+    if number_1 == number_2:
         return "numbers are the same"
-    #
-    if num_1 < num_2:
-        return f"{num_1} is less than {num_2}"
-    return f"{num_1} is greater than {num_2}"
+    elif number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+    else:
+        return f"{number_1} is greater than {number_2}"
