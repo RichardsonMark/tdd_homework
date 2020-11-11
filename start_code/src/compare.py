2baf0facf73b833d53db124756f5382716378d46
Mark Richardson <mark_richardson@sky.com>
2020-11-11 18:18:26 +0000

    Adding test to cover scenario when both numbers are the same and new function to pass the test

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -4,7 +4,9 @@ def compare():
 
 # compare.py
 def compare(num_1, num_2):
+    if num_1 == num_2:
+        return "numbers are the same"
+    #
     if num_1 < num_2:
         return f"{num_1} is less than {num_2}"
-    
     return f"{num_1} is greater than {num_2}"
